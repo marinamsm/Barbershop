f2a7da8f371ae2a9e9c7067ed8a693cdac494164
marinamsm <marinasouza.msm@gmail.com>
2020-08-02 14:46:46 -0300

    Authentication middleware to protect routes

---
+++ src/middlewares/authentication.ts
@@ -0,0 +1,30 @@
+import {Request, Response, NextFunction} from 'express';
+import {verify} from 'jsonwebtoken';
+import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
+
+interface TokenPayload {
+    iat: number;
+    exp: number;
+    sub: string;
+}
+
+export default function authenticate(request: Request, response: Response, next: NextFunction) {
+    const { authorization } = request.headers;
+
+    if(!authorization) {
+        throw new AppError('Missing authorization token', 401);
+    }
+
+    // Bearen tokenifjpeaofpao
+    const [, token] = authorization.split(' ');
+
+    const {sub} = verify(token, authConfig.jwt.secret) as TokenPayload;
+
+    request.user = {
+        id: sub
+    }
+
+    return next();
+
+}
